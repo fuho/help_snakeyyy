de5aadae3ed3c8327a68e50022dbd2ecc0d86668
fuho <ondrej.dolejsi@gmail.com>
2022-05-12 14:36:05 +0200

    pprint req.headers

--- upsell_platform/gunicorn.conf.py
+++ upsell_platform/gunicorn.conf.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+from pprint import pprint
 
 from dynoscale.hooks.gunicorn import pre_request as dynoscale_hook
 
@@ -18,3 +19,4 @@ logging.getLogger("dynoscale").setLevel(logging.DEBUG)
 def pre_request(worker, req):
     # https://dynoscale.net/documentation/getting-started-python
     dynoscale_hook(worker, req)
+    pprint(req.headers)
