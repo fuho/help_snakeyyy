d3b8120335202198b66baab23232e6587d017290
fuho <ondrej.dolejsi@gmail.com>
2022-05-12 16:10:37 +0200

    use Wsgi

--- upsell_platform/wsgi.py
+++ upsell_platform/wsgi.py
@@ -1,5 +1,20 @@
+import logging
+import sys
+
 from dynoscale.wsgi import DynoscaleWsgiApp
 from flask import Flask
+
+handler = logging.StreamHandler(stream=sys.stdout)
+handler.setFormatter(
+    logging.Formatter(
+        fmt="%(asctime)s.%(msecs)03d %(levelname)-8 %(processName)s %(threadName)10s"
+            " %(name)s: %(message)s",
+        datefmt="%H:%M:%S",
+    )
+)
+logging.getLogger("").handlers = [handler]
+logging.getLogger("dynoscale").setLevel(logging.DEBUG)
+
 
 application = Flask(__name__)
 ds_application = DynoscaleWsgiApp(application)
