637d65339417de4cb14295f66ef04261d50182f2
fuho <ondrej.dolejsi@gmail.com>
2022-05-12 13:36:09 +0200

    Moving logginf out of wsgi.py and clarifying logging putput for dynosale in gunicorn.conf.py

--- upsell_platform/gunicorn.conf.py
+++ upsell_platform/gunicorn.conf.py
@@ -1,4 +1,18 @@
+import logging
+import sys
+
 from dynoscale.hooks.gunicorn import pre_request as dynoscale_hook
+
+log_handler = logging.StreamHandler(stream=sys.stdout)
+log_handler.setFormatter(
+    logging.Formatter(
+        fmt="%(asctime)s.%(msecs)03d %(levelname)-8s %(processName)s %(threadName)10s"
+            " %(name)s: %(message)s",
+        datefmt="%H:%M:%S",
+    )
+)
+logging.getLogger("dynoscale").handlers = [log_handler]
+logging.getLogger("dynoscale").setLevel(logging.DEBUG)
 
 
 def pre_request(worker, req):
--- upsell_platform/wsgi.py
+++ upsell_platform/wsgi.py
@@ -2,11 +2,8 @@ import logging
 from flask import Flask
 
 application = Flask(__name__)
-application.logger.setLevel(logging.DEBUG)
 
 
 @application.route("/")
 def index():
-    application.logger.info('################### index requested ###################')
     return "index"
-
